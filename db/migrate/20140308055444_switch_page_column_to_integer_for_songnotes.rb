e1383a971fa369d6a0d1e4f6a3e5f08336bf346e
David English <david@operaonfilm.com>
2014-03-14 20:44:15 -0700

    Postgres change for integer

--- db/migrate/20140308055444_switch_page_column_to_integer_for_songnotes.rb
+++ db/migrate/20140308055444_switch_page_column_to_integer_for_songnotes.rb
@@ -1,9 +1,20 @@
 class SwitchPageColumnToIntegerForSongnotes < ActiveRecord::Migration
-  def self.up
-    change_column :songnotes, :page, 'integer USING CAST(column_name AS integer)'
+
+  def self.up  
+    if connection.adapter_name.downcase == 'postgresql'
+      connection.execute(%q{
+        alter table songnotes
+        alter column page
+        type integer using page::integer
+      })
+    else
+      change_column :songnotes, :page, :integer
+    end
   end
 
+
   def self.down
-    change_column :songnotes, :page, :string
+	 change_column :songnotes, :page, :string
   end
+
 end
