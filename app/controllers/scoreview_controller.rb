93e75fb2f227322a0f9efec312a4ffc16b8407c0
davidenglishmusic <david@operaonfilm.com>
2018-07-22 13:55:09 -0400

    Clean up scoreview and scoreedit controllers

--- app/controllers/scoreview_controller.rb
+++ app/controllers/scoreview_controller.rb
@@ -1,28 +1,19 @@
 class ScoreviewController < ApplicationController
   def show
-    url_array = Piece.find(params[:id]).imageURLs.split("\r\n")
-    @url = url_array[params[:pages].to_i]
+    song_id = params[:id].to_i
+    piece = Piece.find(song_id)
+    @score_page = params[:pages].to_i
 
-    song_component = params[:id].to_i
-    song_page_component = params[:pages].to_i
+    url_array = piece.imageURLs.split("\r\n")
+    @url = url_array[@score_page]
 
-    if params[:pages].to_i >= (url_array.length - 1)
-      @forward_page_url = "/songs/#{song_component}/score_page/#{song_page_component}"
-    else
-      next_page_number = song_page_component + 1
-      @forward_page_url = "/songs/#{song_component}/score_page/#{next_page_number}"
-    end
+    next_score_page = @score_page >= (url_array.length - 1) ? @score_page : @score_page + 1
+    @forward_page_url = "/songs/#{song_id}/score_page/#{next_score_page}"
 
-    if params[:pages].to_i <= 0
-      @previous_page_url = "/songs/#{song_component}/score_page/#{song_page_component}"
-    else
-      previous_page_number = song_page_component - 1
-      @previous_page_url = "/songs/#{song_component}/score_page/#{previous_page_number}"
-    end
+    previous_score_page = @score_page <= 0 ? @score_page : @score_page - 1
+    @previous_page_url = "/songs/#{song_id}/score_page/#{previous_score_page}"
 
-    @song_title = Piece.find(params[:id]).title
-    @page_number = params[:pages].to_i
-
-    @previous_notes = Songnote.where(songtitle: @song_title, page: @page_number)
+    @song_title = piece.title
+    @previous_notes = Songnote.where(songtitle: @song_title, page: @score_page)
   end
 end
